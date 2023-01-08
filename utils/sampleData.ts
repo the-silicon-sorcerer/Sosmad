6f0ad732f2022027bc523fa60f7783d8c48655e9
Michael Kanahele (MAIN) <Kanahelebusiness@gmail.com>
2023-01-08 11:32:59 -0800

    Fixed goofy bug

--- utils/sampleData.ts
+++ utils/sampleData.ts
@@ -17,7 +17,7 @@ export const sampleData: UserData[] = [
         currentStories: [{ storyImage: 'https://images.unsplash.com/photo-1524959725226-f4deb958e979?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80' }]
     },
     {
-        id: 2
+        id: 2,
         username: "Kevin Parks",
         profilePic: "https://images.unsplash.com/photo-1513910367299-bce8d8a0ebf6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=436&q=80",
         currentStories: [{ storyImage: 'https://images.unsplash.com/photo-1524959725226-f4deb958e979?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80' }]
