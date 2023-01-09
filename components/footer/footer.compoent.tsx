453a8571708b9617981c4577a1ee9d5f8831a9f3
Michael Kanahele (MAIN) <Kanahelebusiness@gmail.com>
2023-01-08 19:46:03 -0800

    Fixed bug

--- components/footer/footer.compoent.tsx
+++ components/footer/footer.compoent.tsx
@@ -1,9 +1,22 @@
 import IconButton from '../icon-button/icon-button.component';
 import styles from './footer.module.css'
+import PlusIcon from '../../public/svg/plus.svg'
+import { transferableAbortController } from 'util';
 
 const Footer = () => {
     return (
         <div className={styles.main}>
+            <IconButton SvgIcon={PlusIcon}
+                style={{
+                    backgroundColor: 'var(--accentColor)',
+                    height: '75px',
+                    width: '75px',
+                    position: 'absolute',
+                    top: '0px',
+                    left: '50%',
+                    transform: 'translate(-50%, -50%)'
+
+                }} />
         </div>
     )
 }
